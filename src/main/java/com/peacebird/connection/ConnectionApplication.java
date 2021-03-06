98c47570657693a8ea08b3cbee9afa90691cc140
qdf666 <2430797451@qq.com>
2021-03-06 16:21:57 +0800

    first commit

--- src/main/java/com/peacebird/connection/ConnectionApplication.java
+++ src/main/java/com/peacebird/connection/ConnectionApplication.java
@@ -2,7 +2,7 @@ package com.peacebird.connection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//rush123123
+
 @SpringBootApplication
 public class ConnectionApplication {
     public static void main(String[] args) {
